49b46334079ff7b48a98d1b11cb53003608ad1ce
blastnote <nickbrinthaupt@gmail.com>
2022-01-03 17:07:58 -0500

    prompt adjustment

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const trigger = document.querySelector('.popup-trigger')
 const bodyBlackout = document.querySelector('.body-blackout')
 const popup = document.querySelector('.popup-window')
 const getFacts = document.querySelector('.facts-button')
 
 
-trigger.addEventListener('click', () => {
+setInterval(() => {
     popup.classList.add('is--visible')
     bodyBlackout.classList.add('is-blacked-out')
 
@@ -21,7 +20,7 @@ trigger.addEventListener('click', () => {
         popup.classList.remove('is--visible')
         bodyBlackout.classList.remove('is-blacked-out')
     })
-})
+},3000)
 
 async function getFactsApi() {
     const facts = document.querySelector('.facts')
