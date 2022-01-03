d11832cdd94ea127278dad1d284148bda02e3a7c
blastnote <nickbrinthaupt@gmail.com>
2022-01-03 17:02:35 -0500

    init

---
+++ app.js
@@ -0,0 +1,38 @@
+const trigger = document.querySelector('.popup-trigger')
+const bodyBlackout = document.querySelector('.body-blackout')
+const popup = document.querySelector('.popup-window')
+const getFacts = document.querySelector('.facts-button')
+
+
+trigger.addEventListener('click', () => {
+    popup.classList.add('is--visible')
+    bodyBlackout.classList.add('is-blacked-out')
+
+    getFacts.addEventListener('click',() => {
+        getFactsApi()
+    })
+
+    popup.querySelector('.popup-window-close').addEventListener('click', () => {
+        popup.classList.remove('is--visible')
+        bodyBlackout.classList.remove('is-blacked-out')
+    })
+
+    bodyBlackout.addEventListener('click', () => {
+        popup.classList.remove('is--visible')
+        bodyBlackout.classList.remove('is-blacked-out')
+    })
+})
+
+async function getFactsApi() {
+    const facts = document.querySelector('.facts')
+    await fetch('https://cat-fact.herokuapp.com/facts').then(response =>{
+        return response.json()
+    }).then((json)=>{
+        facts.innerHTML = ''
+        json.forEach((elem,index) => {
+            var line = document.createElement("p")
+            line.innerHTML = (index+1)+". "+elem.text
+            facts.appendChild(line)
+        })
+    })
+}
